945cac1c493aa99df44e4d77325c9d113cbe6d14
renkawan@gmail.com <renkawan@gmail.com@4fb5e150-1ff8-14d0-882d-4bc1c37386cd>
2013-06-28 09:14:01 +0000

    Initial form layout design

--- src/doxy/Doxy.java
+++ src/doxy/Doxy.java
@@ -4,6 +4,11 @@
  */
 package doxy;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 /**
  *
  * @author Rendra
@@ -15,5 +20,12 @@ public class Doxy {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            Main mn = new Main();
+            mn.setVisible(true);
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, e);
+        }
     }
 }
