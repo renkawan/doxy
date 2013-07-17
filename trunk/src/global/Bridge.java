6c96e5d2ec3f15b1df3e49ade05517b1d0b96801
renkawan@gmail.com <renkawan@gmail.com@4fb5e150-1ff8-14d0-882d-4bc1c37386cd>
2013-07-17 19:10:27 +0000

    Changelog : - Now suport parse file for further analyzing - Readfile - Get line number from regex offset - And much more

--- trunk/src/global/Bridge.java
+++ trunk/src/global/Bridge.java
@@ -6,13 +6,18 @@ package global;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 /**
  *
  * @author Rendra
  */
 public class Bridge {
-    private List<String> listJavaSrc = new ArrayList<String>();
+    private List<String> listJavaSrc = new ArrayList<>();
+    private MyVector listSources;
+    private String selectedSrcFile;
+    private int totalLineOfCode;
+    
     public void setWorkPath(String path){
         DoxyApp.workPath = path;
     }
@@ -21,5 +26,34 @@ public class Bridge {
     }
     public List<String> getListJavaSrc(){
         return listJavaSrc;
+    }
+    public void setListSources(String path) {
+        if(listSources==null) {
+            listSources = new MyVector(path);
+        } else {
+            listSources.add(path);
+        }
+    }
+    public MyVector getListSources() {
+        return this.listSources;
+    }
+    public void setSelectedSrcFile(String file) {
+        this.selectedSrcFile = file;
+    }
+    public String getSelectedSrcFile() {
+        return this.selectedSrcFile;
+    }
+    public void addLineOfCode() {
+        totalLineOfCode++;
+    }
+    public int getLineOfCode() {
+        return totalLineOfCode;
+    }
+    private int countLineOfComments(String comment){
+        comment = Pattern.compile("\\s*$", Pattern.MULTILINE).matcher(comment).replaceAll("");
+        return comment.split("\n").length;
+    }
+    public int getCommentsLine(String comment) {
+        return countLineOfComments(comment);
     }
 }
--- trunk/src/kit/FileKit.java
+++ trunk/src/kit/FileKit.java
@@ -6,8 +6,13 @@ package kit;
 
 import global.DoxyApp;
 import global.MyVector;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileFilter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  *
@@ -35,9 +40,59 @@ public class FileKit {
                 vectorDirectory.add(vector);
             } else {
                 vectorDirectory.add(file.getName());
-                DoxyApp.bridge.setListJavaItem(file.getAbsolutePath());
+                DoxyApp.bridge.setListSources(file.getAbsolutePath());
             }
         }
         return vectorDirectory;
+    }
+    
+    /**
+     * Read selected java source file and save into String for further operation
+     *
+     * @param file_location
+     * @return 
+     */
+    public static String readSrcFile(String file_location) {
+        try {
+            File src = new File(file_location);
+            FileInputStream fis = new FileInputStream(src);
+            BufferedReader buffer = new BufferedReader(new InputStreamReader(fis));
+            StringBuilder text = new StringBuilder((int)src.length());
+            String line = null;
+            while((line = buffer.readLine()) != null) {
+                text.append(line).append("\n");
+                DoxyApp.bridge.addLineOfCode();
+            }
+            return text.toString();
+        }
+        catch (FileNotFoundException notfound) { return notfound.toString(); }
+        catch (IOException ioex) { return ioex.toString(); }
+    }
+    
+    /**
+     * Function for getting line number from the needle character position
+     * in a large String.
+     * 
+     * The needle is a offset (start / end) position
+     * which got from regex matcher operation.
+     * 
+     * @param offset
+     * @param contents
+     * @return 
+     */
+    public static int getNumbLine(int offset, String[] contents) {
+        int temp = 0;
+        int i = 0;
+        for(i=0;i<contents.length;i++) {
+            temp = temp + contents[i].length() + 1;
+            if (temp>=offset)
+                return i+1;
+        }
+        return offset;
+    }
+    
+    public static String getFileName(String full_path) {
+        String[] explode = full_path.replace("\\", "/").split("/");
+        return explode[explode.length-1];
     }
 }
