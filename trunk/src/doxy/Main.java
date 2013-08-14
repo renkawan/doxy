d76ee36d22ee7dc7d14198e4832c83065b54397a
renkawan@gmail.com <renkawan@gmail.com@4fb5e150-1ff8-14d0-882d-4bc1c37386cd>
2013-08-14 03:13:06 +0000

    Changelog : - Bugs fixed

--- trunk/src/doxy/Main.java
+++ trunk/src/doxy/Main.java
@@ -23,8 +23,6 @@ import java.beans.PropertyVetoException;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -684,16 +682,6 @@ public class Main extends javax.swing.JFrame {
         } catch (IOException ex) {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
         }
-    }
-    
-    public static void pipeStream(InputStream input, OutputStream output) throws IOException {
-        byte buffer[] = new byte[1024];
-        int numRead;
-        do {
-            numRead = input.read(buffer);
-            output.write(buffer, 0, numRead);
-        } while (input.available() > 0);
-        output.flush();
     }
     
     private void openMainImage() {
