3232dd34f15c8cc2d03720072e4a0d85d85da790
renkawan@gmail.com <renkawan@gmail.com@4fb5e150-1ff8-14d0-882d-4bc1c37386cd>
2013-08-28 07:24:44 +0000

    Changelog : - Support detect Encapsulation, Overriding and Overloading Method - More bugs fixed

--- src/global/DoxyApp.java
+++ src/global/DoxyApp.java
@@ -9,16 +9,22 @@ package global;
  * @author Rendra
  */
 public class DoxyApp {
-    public static Bridge bridge = new Bridge();    
-    public static String workPath;    
-    public final static String rxComment = "/\\*(?>(?:(?>[^*]+)|\\*(?!/))*)\\*/|(//.*)";    
-    public final static String rxMethod = "\\w+ +\\w+ *\\([^\\)]*\\) *\\{";    
-    public final static String rxEmptyLine = "^\\s*$";    
-    public final static String myServer = "http://localhost/2013/doxy/";
-    public final static String myWiki = "http://code.google.com/p/doxy/";    
-    public final static String doxletpath = "D:\\Doclava\\doclava.jar";    
-    public final static String doxlet = "com.google.doclava.Doclava";
     public static int progressCounter;
-    public final static String myAppName = "Doxy";
-    public final static String myVersion = "2.0.1";
+    public static String workPath;
+    
+    public static Bridge bridge             = new Bridge();    
+    public final static String rxComment    = "/\\*(?>(?:(?>[^*]+)|\\*(?!/))*)\\*/|(//.*)";    
+    public final static String rxMethod     = "\\w+ +\\w+ *\\([^\\)]*\\) *\\{";
+    public final static String allMethod    = "((public|protected|private|\\s) [\\w\\<\\>\\[\\]]+\\s+(\\w+) *\\([^\\)]*\\) *(\\{?|[^;]))|"+
+            "(\\w+ \\w+ \\w+ +\\w+ *\\([^\\)]*\\) *\\{)";
+    public final static String encMethod    = "((protected|private) +\\w+ [\\w\\<\\>\\[\\]]+\\s+(\\w+) *\\([^\\)]*\\) *(\\{?|[^;]))|"+
+            "((protected|private) +[\\w\\<\\>\\[\\]]+\\s+(\\w+) *\\([^\\)]*\\) *(\\{?|[^;]))";
+    public final static String ovrMethod    = "@Override\\s*(.*)";
+    public final static String rxEmptyLine  = "^\\s*$";    
+    public final static String myServer     = "http://localhost/2013/doxy/";
+    public final static String myWiki       = "http://code.google.com/p/doxy/";    
+    public final static String doxletpath   = "D:\\Doclava\\doclava.jar";    
+    public final static String doxlet       = "com.google.doclava.Doclava";
+    public final static String myAppName    = "Doxy";
+    public final static String myVersion    = "2.0.1";
 }
